59296facc498107510decd7d07c288f6783f2af7
Sreephaneesha <kanugovisreephaneesha@gmail.com>
2024-10-31 22:41:54 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Createpost from './pages/Createpost'
 
